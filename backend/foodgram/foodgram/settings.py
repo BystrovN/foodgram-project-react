1027faefeed589938af2a7ce6423acd1330287fc
Никита Быстров <hallnik96@gmail.com>
2023-02-06 22:56:39 +0300

    Dev 0.7 review is done, django secret key add to env

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 DEBUG = False
 
